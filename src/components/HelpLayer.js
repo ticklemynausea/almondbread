456f0c39b418b2645767f7a70da7483ae23b4126
Mário Carneiro <emailoftheyear@gmail.com>
2021-09-01 18:28:47 +0100

    Rework Help screen

--- src/components/HelpLayer.js
+++ src/components/HelpLayer.js
@@ -1,71 +1,80 @@
 import "HelpLayer.scss";
 
+const README_URL = "https://github.com/ticklemynausea/almondbread/blob/master/README.md";
+const BLOGPOST_URL = "https://mariocarneiro.info/2021/08/30/almondbread-another-mandelbrot-fractal-renderer.html";
+
 const HelpLayer = ({ toggleHelp }) => (
   <div id="help" className="help-overlay fullscreen-overlay" onClick={toggleHelp}>
     <div className="help-overlay__inside">
       <div className="help-overlay__inside__top">
         <h1>Almondbread</h1>
-        <p>Yet another mandelbrot fractal explorer, with pretty colors and nifty features.</p>
-        <p>No touch controls available yet, a computer with keyboard and mouse is needed for now.</p>
+        <p>
+          A mandelbrot fractal explorer, with pretty colors and nifty features.
+          It is also a work in progress.
+        </p>
+        <p>
+          Read more about it in the project's <a href={README_URL}>README</a>, and also <a href={BLOGPOST_URL}>this blog post</a>.
+        </p>
       </div>
 
+      <div className="help-overlay__inside__mid">
+        <h1>Controls</h1>
+        <div className="help-overlay__inside__mid-left">
+          <dl>
+            <dt>Arrows</dt>
+            <dd>Pan around</dd>
 
-      <h1>Controls</h1>
-      <div className="help-overlay__inside__mid-left">
-        <dl>
-          <dt>Arrows</dt>
-          <dd>Pan around</dd>
+            <dt>Q / W</dt>
+            <dd>Zoom in/out</dd>
 
-          <dt>Q / W</dt>
-          <dd>Zoom in/out</dd>
+            <dt>D / F</dt>
+            <dd>Horizontal scale - / +</dd>
 
-          <dt>D / F</dt>
-          <dd>Horizontal scale - / +</dd>
+            <dt>C / V</dt>
+            <dd>Vertical scale - / +</dd>
 
-          <dt>C / V</dt>
-          <dd>Vertical scale - / +</dd>
+            <dt>T / Y</dt>
+            <dd>Palette selection</dd>
 
-          <dt>T / Y</dt>
-          <dd>Palette selection</dd>
+            <dt>U</dt>
+            <dd>Toggle coloring method</dd>
+          </dl>
+        </div>
 
-          <dt>U</dt>
-          <dd>Toggle coloring method</dd>
-        </dl>
+        <div className="help-overlay__inside__mid-right">
+          <dl>
+            <dt>Selection</dt>
+            <dd>Zoom on selected area</dd>
+
+            <dt>A / S</dt>
+            <dd>Max iterations - / +</dd>
+
+            <dt>Z / X</dt>
+            <dd>Web workers - / +</dd>
+
+            <dt>E</dt>
+            <dd>Undo last action</dd>
+
+            <dt>R</dt>
+            <dd>Reset to initial state</dd>
+
+            <dt>F</dt>
+            <dd>Download image</dd>
+          </dl>
+        </div>
+
+        <p className="center">Press <span className="key">H</span> to toggle this overlay</p>
       </div>
-
-      <div className="help-overlay__inside__mid-right">
-        <dl>
-          <dt>Selection</dt>
-          <dd>Zoom on selected area</dd>
-
-          <dt>A / S</dt>
-          <dd>Max iterations - / +</dd>
-
-          <dt>Z / X</dt>
-          <dd>Web workers - / +</dd>
-
-          <dt>E</dt>
-          <dd>Undo last action</dd>
-
-          <dt>R</dt>
-          <dd>Reset to initial state</dd>
-
-          <dt>F</dt>
-          <dd>Download image</dd>
-        </dl>
-      </div>
-
-      <p className="center">Press <span className="key">H</span> to toggle this overlay</p>
 
       <div className="help-overlay__inside__bottom">
         <span>
           <a href="http://github.com/ticklemynausea/almondbread">source code</a>
         </span>
         <span>
-          <a href="http://mariocarneiro.info">mariocarneiro.info</a>
+          <a href="http://mariocarneiro.info/blog.html">tech blog</a>
         </span>
         <span>
-          mandelbrot is german for almond bread
+          <a href="http://mariocarneiro.info">mariocarneiro.info</a>
         </span>
       </div>
     </div>
